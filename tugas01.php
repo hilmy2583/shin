7572cce5ed5d2a4228ad7c24482264e2bff65ab6
hilmy2583 <70121101+hilmy2583@users.noreply.github.com>
2020-08-24 09:43:33 +0700

    Add files via upload

---
+++ tugas01.php
@@ -0,0 +1,20 @@
+<?php
+
+class hewan{
+    var $nama;
+    var $jenis;
+
+    function nama_hewan(){
+        return "Hewan itu adalah Gajah<br/>";
+    }
+    function jenis_hewan(){
+        return "Hewan itu berjenis mamalia/menyusui/melahirkan<br/>";
+    }
+
+$hewan = new hewan();
+echo $hewan->nama_hewan();
+echo $hewan->jenis_hewan();
+
+}
+
+?>
